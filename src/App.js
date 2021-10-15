a1b986b7c64c35fffe3f0fada2fce66692d2f547
unknown <example@gmail.com>
2021-10-16 00:17:02 +0500

    new Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router}from 'react-router-dom';
+import Header from './components/Header';
+import Section from './components/Section';
+import { DataProvider } from './components/Context';
+import { ToastContainer } from 'react-toastify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends React.Component{
+    render(){
+        return(
+            <DataProvider>
+            <div className="app">
+                <ToastContainer 
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                dark
+                className="ToasT"/>
+                <Router>
+                    <Header />
+                    <Section/>
+                </Router>
+            </div>
+            </DataProvider>
+
+        )
+    }
 }
 
-export default App;
---
+++ src/components/section/Payments.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Payments extends Component {
+    render() {
+        return (
+            <div>
+                <h2 style={{textAlign:"center"}}>payment</h2>
+            </div>
+        )
+    }
+}
